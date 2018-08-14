c0c7f1af9eefcb2f72653c54c0b7715d704b8452
maska <maska@mail.ru>
2018-08-14 18:51:19 +0300

    VLabs: Valter links; valter torsoToHead cable sleeve animation

--- vl.vlabs.com.ua/vlabs/src/vlabs.items/valter/index.js
+++ vl.vlabs.com.ua/vlabs/src/vlabs.items/valter/index.js
@@ -1,5 +1,6 @@
 import * as THREE           from 'three';
 import * as THREEUtils      from '../../vlabs.core/utils/three.utils';
+import * as dat             from 'dat.gui';
 var TransformControls       = require('../../vlabs.core/three-transformcontrols/index');
 
 export default class Valter {
@@ -18,7 +19,27 @@ initObj {
         this.name = this.initObj.name;
         }
 
-       this.initialize();
+        var textureLoader = new THREE.TextureLoader();
+
+        return new Promise((resolve, reject) => {
+            Promise.all([
+                textureLoader.load('../vlabs.items/valter/textures/dynamic/carbon_fibre.jpg')
+            ])
+            .then((result) => {
+                var cableSleeveMaterialTexture = result[0];
+                cableSleeveMaterialTexture.wrapS = cableSleeveMaterialTexture.wrapT = THREE.RepeatWrapping;
+                cableSleeveMaterialTexture.repeat.set(4, 1);
+                this.cableSleeveMaterial = new THREE.MeshLambertMaterial({
+                    wireframe: false,
+                    flatShading: false,
+                    map: cableSleeveMaterialTexture
+                });
+
+                this.initialize();
+            });
+        });
+
+    //    this.initialize();
     }
 
     initialize() {
@@ -33,35 +54,13 @@ initObj {
                 console.error("Valter is not set");
             }
 
-            this.cableSleeveMaterial = new THREE.LineBasicMaterial( { color : 0xff0000 } );
-
             this.baseFrame      = this.model.getObjectByName('baseFrame');
             this.bodyFrame      = this.model.getObjectByName('bodyFrame');
             this.torsoFrame     = this.model.getObjectByName('torsoFrame');
-            this.headTiltLink   = this.model.getObjectByName('headTiltLink');
             this.headFrame      = this.model.getObjectByName('headFrame');
 
-            this.torsoFrameCableInput = new THREE.Object3D();
-            this.torsoFrameCableInput.position.set(0.0, -0.101, 0.38);
-            this.torsoFrame.add(this.torsoFrameCableInput);
-
-            this.headFrameCableInput = new THREE.Object3D();
-            this.headFrameCableInput.position.set(0.0, 0.1036, 0.057);
-            this.headFrame.add(this.headFrameCableInput);
-
-            this.model.updateMatrixWorld();
-            this.torsoFrameToHeadFrameCableCurve = new THREE.CatmullRomCurve3([
-                this.torsoFrame.worldToLocal(THREEUtils.getWorldPosition(this.torsoFrameCableInput)),
-                this.torsoFrame.worldToLocal(THREEUtils.getWorldPosition(this.torsoFrameCableInput)).add(new THREE.Vector3(0.0, -0.0175, 0.025)),
-                this.torsoFrame.worldToLocal(THREEUtils.getWorldPosition(this.torsoFrameCableInput)).add(new THREE.Vector3(0.0, -0.075, 0.15)),
-                this.torsoFrame.worldToLocal(THREEUtils.getWorldPosition(this.headFrameCableInput)).add(new THREE.Vector3(0.0, -0.02, 0.0)),
-                this.torsoFrame.worldToLocal(THREEUtils.getWorldPosition(this.headFrameCableInput))
-            ]);
-
-            this.torsoFrameToHeadFrameCableGeometry = new THREE.TubeBufferGeometry(this.torsoFrameToHeadFrameCableCurve, 12, 0.01, 5);
-            this.torsoFrameToHeadFrameCableMesh = new THREE.Mesh(this.torsoFrameToHeadFrameCableGeometry, this.cableSleeveMaterial);
-            this.torsoFrame.add(this.torsoFrameToHeadFrameCableMesh);
-
+            this.headTiltLink   = this.model.getObjectByName('headTiltLink');
+            this.headYawLink    = this.model.getObjectByName('headYawLink');
 
 
             if (this.initObj.manipulation) {
@@ -70,8 +69,87 @@ initObj {
                 this.context.vLabScene.add(this.manipulationControl);
                 this.context.manipulationControl.attach(this.model);
             }
+
+            this.initializeLinks();
+            this.initializeCableSleeves();
+            this.setupDatGUI()
         }).catch(error => {
             console.error(error);
         });
+    }
+
+    initializeLinks() {
+        this.ValterLinks = {
+            headTiltLink: {
+                value: this.headTiltLink.rotation.x,
+                min: 0.0,
+                max: 0.95,
+                step: 0.01
+            },
+            headYawLink: {
+                value: this.headYawLink.rotation.z,
+                min: -1.1,
+                max: 1.1,
+                step: 0.01
+            },
+            torsoFrame: {
+                value: this.torsoFrame.rotation.x,
+                min: 0.0,
+                max: 0.8,
+                step: 0.01
+            }
+        };
+        console.log(this.ValterLinks);
+    }
+
+    initializeCableSleeves() {
+        this.torsoFrameCableInput = new THREE.Object3D();
+        this.torsoFrameCableInput.position.set(0.0, -0.101, 0.38);
+        this.torsoFrame.add(this.torsoFrameCableInput);
+
+        this.headFrameCableInput = new THREE.Object3D();
+        this.headFrameCableInput.position.set(0.0, 0.1036, 0.057);
+        this.headFrame.add(this.headFrameCableInput);
+
+        this.torsoFrameToHeadFrameSleeveSegments = 16;
+
+        this.torsoFrameToHeadFrameCableGeometry = new THREE.TubeBufferGeometry(this.getTorsoFrameToHeadFrameCableCurve(), this.torsoFrameToHeadFrameSleeveSegments, 0.01, 5);
+        this.torsoFrameToHeadFrameCableMesh = new THREE.Mesh(this.torsoFrameToHeadFrameCableGeometry, this.cableSleeveMaterial);
+        this.torsoFrame.add(this.torsoFrameToHeadFrameCableMesh);
+    }
+
+    setupDatGUI() {
+        this.datGUI = new dat.GUI();
+        this.datGUI.add(this.ValterLinks.headTiltLink, 'value', this.ValterLinks.headTiltLink.min, this.ValterLinks.headTiltLink.max, this.ValterLinks.headTiltLink.step).name('Head Tilt Link').step(0.01).onChange(this.setHeadTiltLink.bind(this));
+        this.datGUI.add(this.ValterLinks.headYawLink, 'value', this.ValterLinks.headYawLink.min, this.ValterLinks.headYawLink.max, this.ValterLinks.headYawLink.step).name('Head Yaw Link').step(0.01).onChange(this.setHeadYawLink.bind(this));
+        this.datGUI.add(this.ValterLinks.torsoFrame, 'value', this.ValterLinks.torsoFrame.min, this.ValterLinks.torsoFrame.max, this.ValterLinks.torsoFrame.step).name('Torso Tilt').step(0.01).onChange(this.setTorsoFrame.bind(this));
+    }
+
+    setHeadTiltLink(value) {
+        this.ValterLinks.headTiltLink.value = value;
+        this.headTiltLink.rotation.x = this.ValterLinks.headTiltLink.value * -1;
+        this.torsoFrameToHeadFrameCableGeometry.copy(new THREE.TubeBufferGeometry(this.getTorsoFrameToHeadFrameCableCurve(), this.torsoFrameToHeadFrameSleeveSegments, 0.01, 5));
+    }
+
+    setHeadYawLink(value) {
+        this.ValterLinks.headYawLink.value = value;
+        this.headYawLink.rotation.z = this.ValterLinks.headYawLink.value;
+        this.torsoFrameToHeadFrameCableGeometry.copy(new THREE.TubeBufferGeometry(this.getTorsoFrameToHeadFrameCableCurve(), this.torsoFrameToHeadFrameSleeveSegments, 0.01, 5));
+    }
+
+    getTorsoFrameToHeadFrameCableCurve() {
+        this.model.updateMatrixWorld();
+        return new THREE.CatmullRomCurve3([
+            this.torsoFrame.worldToLocal(THREEUtils.getWorldPosition(this.torsoFrameCableInput)).add(new THREE.Vector3(0.0, 0.01, -0.01)),
+            this.torsoFrame.worldToLocal(THREEUtils.getWorldPosition(this.torsoFrameCableInput)).add(new THREE.Vector3(0.0, -0.018, 0.025)),
+            this.torsoFrame.worldToLocal(THREEUtils.getWorldPosition(this.torsoFrameCableInput)).add(new THREE.Vector3(this.ValterLinks.headYawLink.value / 30, -0.075 + this.ValterLinks.headTiltLink.value / 7 + Math.abs(this.ValterLinks.headYawLink.value / 60), 0.15 + this.ValterLinks.headTiltLink.value / 15)),
+            this.torsoFrame.worldToLocal(THREEUtils.getWorldPosition(this.headFrameCableInput)).add(new THREE.Vector3(this.ValterLinks.headYawLink.value / 50, -0.02, -0.002 + this.ValterLinks.headTiltLink.value / 50 + Math.abs(this.ValterLinks.headYawLink.value / 50) * this.ValterLinks.headTiltLink.value)),
+            this.torsoFrame.worldToLocal(THREEUtils.getWorldPosition(this.headFrameCableInput)).add(new THREE.Vector3(-this.ValterLinks.headYawLink.value / 40, 0.02, -0.002 - this.ValterLinks.headTiltLink.value / 50 - Math.abs(this.ValterLinks.headYawLink.value / 30) * this.ValterLinks.headTiltLink.value ))
+        ]);
+    }
+
+    setTorsoFrame(value) {
+        this.ValterLinks.torsoFrame.value = value;
+        this.torsoFrame.rotation.x = this.ValterLinks.torsoFrame.value * -1;
     }
 }
