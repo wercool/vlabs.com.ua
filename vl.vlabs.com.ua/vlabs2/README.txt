f533389f608af361daa7322ebf04418592a766a9
maska <wercool.in.ua@gmail.com>
2018-10-11 03:00:49 +0300

    Vlabs2: minor

--- vl.vlabs.com.ua/vlabs2/README.txt
+++ vl.vlabs.com.ua/vlabs2/README.txt
@@ -5,3 +5,6 @@ gulp --vlab vlab.base --mode dev
 
 VLab cache:
 <html manifest="./resources/vlab.appcache">
+
+Text to ASCII image (Font: Big)
+http://patorjk.com/software/
--- vl.vlabs.com.ua/vlabs2/src/vlab.fwk/core/vlab.scene.interactable.js
+++ vl.vlabs.com.ua/vlabs2/src/vlab.fwk/core/vlab.scene.interactable.js
@@ -163,6 +163,20 @@ class VLabSceneInteractable {
      * Initialize VLabSceneInteractable.
      * Adds this VLabSceneInteractable to {@link VLabScene#interactables};
      *
+     * 
+     * 
+     * 
+     *   _____           __      _         
+     *  |  __ \         / _|    | |        
+     *  | |__) | __ ___| |_ __ _| |__  ___ 
+     *  |  ___/ '__/ _ \  _/ _` | '_ \/ __|
+     *  | |   | | |  __/ || (_| | |_) \__ \
+     *  |_|   |_|  \___|_| \__,_|_.__/|___/
+     * 
+     * 
+     * 
+     * 
+     * 
      * @memberof VLabSceneInteractable
      * @async
      * @returns {Promise | VLabSceneInteractable}                       - VLabSceneInteractable instance in Promise resolver
