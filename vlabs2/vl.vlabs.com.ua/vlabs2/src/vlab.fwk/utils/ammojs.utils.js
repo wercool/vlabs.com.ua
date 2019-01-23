6c02762096890804c373efd20d25b563c67d0dd1
maska <maska@mail.ru>
2019-01-23 14:34:24 +0200

    VLabs2: ammojs exps

---
+++ vlabs2/vl.vlabs.com.ua/vlabs2/src/vlab.fwk/utils/ammojs.utils.js
@@ -0,0 +1,187 @@
+import * as THREE from 'three';
+
+export function ammoBoxShapeBody(params) {
+    params.mesh.geometry.computeBoundingBox();
+    let meshBoundingBoxGeometry = new THREE.BoxGeometry(
+        params.mesh.geometry.boundingBox.max.x - params.mesh.geometry.boundingBox.min.x, 
+        params.mesh.geometry.boundingBox.max.y - params.mesh.geometry.boundingBox.min.y, 
+        params.mesh.geometry.boundingBox.max.z - params.mesh.geometry.boundingBox.min.z, 
+        1, 1, 1);
+    meshBoundingBoxGeometry.computeBoundingBox();
+
+    let w = (meshBoundingBoxGeometry.boundingBox.max.x - meshBoundingBoxGeometry.boundingBox.min.x) * 0.5;
+    let h = (meshBoundingBoxGeometry.boundingBox.max.y - meshBoundingBoxGeometry.boundingBox.min.y) * 0.5;
+    let l = (meshBoundingBoxGeometry.boundingBox.max.z - meshBoundingBoxGeometry.boundingBox.min.z) * 0.5;
+
+    if (params.boxMargins) {
+        w += params.boxMargins.x;
+        h += params.boxMargins.y;
+        l += params.boxMargins.z;
+    }
+
+    let ammoShapeGeometry = new Ammo.btBoxShape(new Ammo.btVector3(w, h, l));
+    let transform = new Ammo.btTransform();
+    transform.setIdentity();
+    transform.setOrigin(new Ammo.btVector3(params.mesh.position.x, params.mesh.position.y, params.mesh.position.z));
+    transform.setRotation(new Ammo.btQuaternion(params.mesh.quaternion.x, params.mesh.quaternion.y, params.mesh.quaternion.z, params.mesh.quaternion.w));
+    let motionState = new Ammo.btDefaultMotionState(transform);
+
+    let localInertia = new Ammo.btVector3(0.0, 0.0, 0.0);
+    ammoShapeGeometry.calculateLocalInertia(0.1, localInertia);
+
+    let rbInfo = new Ammo.btRigidBodyConstructionInfo(params.mass, motionState, ammoShapeGeometry, localInertia);
+    let ammoBody = new Ammo.btRigidBody(rbInfo);
+    if (params.friction) ammoBody.setFriction(params.friction);
+    if (params.restitution) ammoBody.setRestitution(params.restitution);
+    if (params.damping) ammoBody.setDamping(params.damping);
+
+    if (params.mass > 0.0 && params.deactivation) {
+        ammoBody.setActivationState(params.deactivation);
+    }
+
+    return {
+        body: ammoBody,
+        mesh: params.mesh,
+        mass: params.mass
+    }
+}
+
+export class AmmoJSRaycastVehicle {
+    constructor(initObj) {
+        this.initObj = initObj;
+        console.log(this.initObj);
+
+                var materialInteractive = new THREE.MeshPhongMaterial( { color:0x990000 } );
+
+				// Vehicle contants
+				var chassisWidth = 1.8;
+				var chassisHeight = .6;
+				var chassisLength = 4;
+				var massVehicle = 800;
+				var wheelAxisPositionBack = -1;
+				var wheelRadiusBack = .4;
+				var wheelWidthBack = .3;
+				var wheelHalfTrackBack = 1;
+				var wheelAxisHeightBack = .3;
+				var wheelAxisFrontPosition = 1.7;
+				var wheelHalfTrackFront = 1;
+				var wheelAxisHeightFront = .3;
+				var wheelRadiusFront = .35;
+				var wheelWidthFront = .2;
+				var friction = 1000;
+				var suspensionStiffness = 20.0;
+				var suspensionDamping = 2.3;
+				var suspensionCompression = 4.4;
+				var suspensionRestLength = 0.6;
+				var rollInfluence = 0.2;
+				var steeringIncrement = .04;
+				var steeringClamp = .5;
+				var maxEngineForce = 2000;
+				var maxBreakingForce = 100;
+
+                function createChassisMesh(w, l, h) {
+                    var shape = new THREE.BoxGeometry(w, l, h, 1, 1, 1);
+                    var mesh = new THREE.Mesh(shape, materialInteractive);
+                    initObj.vLabScene.add(mesh);
+                    return mesh;
+                }
+
+				// Chassis
+				var geometry = new Ammo.btBoxShape(new Ammo.btVector3(chassisWidth * .5, chassisHeight * .5, chassisLength * .5));
+				var transform = new Ammo.btTransform();
+				transform.setIdentity();
+				transform.setOrigin(new Ammo.btVector3(this.initObj.vehiclePos.x, this.initObj.vehiclePos.y, this.initObj.vehiclePos.z));
+				transform.setRotation(new Ammo.btQuaternion(this.initObj.vehicleQuat.x, this.initObj.vehicleQuat.y, this.initObj.vehicleQuat.z, this.initObj.vehicleQuat.w));
+				var motionState = new Ammo.btDefaultMotionState(transform);
+				var localInertia = new Ammo.btVector3(0, 0, 0);
+				geometry.calculateLocalInertia(massVehicle, localInertia);
+				var body = new Ammo.btRigidBody(new Ammo.btRigidBodyConstructionInfo(massVehicle, motionState, geometry, localInertia));
+				body.setActivationState(4);
+				this.initObj.physicsConfiguration.physicsWorld.addRigidBody(body);
+				var chassisMesh = createChassisMesh(chassisWidth, chassisHeight, chassisLength);
+
+				// Raycast Vehicle
+				var engineForce = 0;
+				var vehicleSteering = 0;
+				var breakingForce = 0;
+				var tuning = new Ammo.btVehicleTuning();
+				var rayCaster = new Ammo.btDefaultVehicleRaycaster(this.initObj.physicsConfiguration.physicsWorld);
+				var vehicle = new Ammo.btRaycastVehicle(tuning, body, rayCaster);
+				vehicle.setCoordinateSystem(0, 1, 2);
+				this.initObj.physicsConfiguration.physicsWorld.addAction(vehicle);
+
+				// Wheels
+				var wheelMeshes = [];
+				var FRONT_LEFT = 0;
+				var FRONT_RIGHT = 1;
+				var BACK_LEFT = 2;
+				var BACK_RIGHT = 3;
+				var wheelMeshes = [];
+				var wheelDirectionCS0 = new Ammo.btVector3(0, -1, 0);
+				var wheelAxleCS = new Ammo.btVector3(-1, 0, 0);
+
+                function createWheelMesh(radius, width) {
+                    var t = new THREE.CylinderGeometry(radius, radius, width, 24, 1);
+                    t.rotateZ(Math.PI / 2);
+                    var mesh = new THREE.Mesh(t, materialInteractive);
+                    mesh.add(new THREE.Mesh(new THREE.BoxGeometry(width * 1.5, radius * 1.75, radius*.25, 1, 1, 1), materialInteractive));
+                    initObj.vLabScene.add(mesh);
+                    return mesh;
+                }
+
+				function addWheel(isFront, pos, radius, width, index) {
+					var wheelInfo = vehicle.addWheel(
+							pos,
+							wheelDirectionCS0,
+							wheelAxleCS,
+							suspensionRestLength,
+							radius,
+							tuning,
+							isFront);
+					wheelInfo.set_m_suspensionStiffness(suspensionStiffness);
+					wheelInfo.set_m_wheelsDampingRelaxation(suspensionDamping);
+					wheelInfo.set_m_wheelsDampingCompression(suspensionCompression);
+					wheelInfo.set_m_frictionSlip(friction);
+					wheelInfo.set_m_rollInfluence(rollInfluence);
+					wheelMeshes[index] = createWheelMesh(radius, width);
+				}
+
+				addWheel(true, new Ammo.btVector3(wheelHalfTrackFront, wheelAxisHeightFront, wheelAxisFrontPosition), wheelRadiusFront, wheelWidthFront, FRONT_LEFT);
+				addWheel(true, new Ammo.btVector3(-wheelHalfTrackFront, wheelAxisHeightFront, wheelAxisFrontPosition), wheelRadiusFront, wheelWidthFront, FRONT_RIGHT);
+				addWheel(false, new Ammo.btVector3(-wheelHalfTrackBack, wheelAxisHeightBack, wheelAxisPositionBack), wheelRadiusBack, wheelWidthBack, BACK_LEFT);
+				addWheel(false, new Ammo.btVector3(wheelHalfTrackBack, wheelAxisHeightBack, wheelAxisPositionBack), wheelRadiusBack, wheelWidthBack, BACK_RIGHT);
+
+                this.sync = function() {
+
+					breakingForce = 0;
+					engineForce = 0;
+
+					vehicle.applyEngineForce(engineForce, BACK_LEFT);
+					vehicle.applyEngineForce(engineForce, BACK_RIGHT);
+					vehicle.setBrake(breakingForce / 2, FRONT_LEFT);
+					vehicle.setBrake(breakingForce / 2, FRONT_RIGHT);
+					vehicle.setBrake(breakingForce, BACK_LEFT);
+					vehicle.setBrake(breakingForce, BACK_RIGHT);
+					vehicle.setSteeringValue(vehicleSteering, FRONT_LEFT);
+					vehicle.setSteeringValue(vehicleSteering, FRONT_RIGHT);
+
+					var tm, p, q, i;
+					var n = vehicle.getNumWheels();
+					for (i = 0; i < n; i++) {
+						vehicle.updateWheelTransform(i, true);
+						tm = vehicle.getWheelTransformWS(i);
+						p = tm.getOrigin();
+						q = tm.getRotation();
+						wheelMeshes[i].position.set(p.x(), p.y(), p.z());
+						wheelMeshes[i].quaternion.set(q.x(), q.y(), q.z(), q.w());
+					}
+
+
+					tm = vehicle.getChassisWorldTransform();
+					p = tm.getOrigin();
+					q = tm.getRotation();
+					chassisMesh.position.set(p.x(), p.y(), p.z());
+					chassisMesh.quaternion.set(q.x(), q.y(), q.z(), q.w());
+                }
+    }
+}
--- vlabs2/vl.vlabs.com.ua/vlabs2/src/vlabs/toys.kinsmart.lamborghiniMiura1971P400SV/scenes/base.scene/index.js
+++ vlabs2/vl.vlabs.com.ua/vlabs2/src/vlabs/toys.kinsmart.lamborghiniMiura1971P400SV/scenes/base.scene/index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import VLabScene from '../../../../vlab.fwk/core/vlab.scene';
-import * as AmmoJSUTILS from '../../../../vlab.fwk/utils/ammo.utils';
+import * as AmmoJSUTILS from '../../../../vlab.fwk/utils/ammojs.utils';
 
 class BaseScene extends VLabScene {
     constructor(iniObj) {
@@ -28,15 +28,22 @@ class BaseScene extends VLabScene {
         this.ambientLight = new THREE.AmbientLight(0x404040, 0.5);
         this.add(this.ambientLight);
 
-        this.pointLight = new THREE.PointLight(0xffffff, 2.0);
+        this.pointLight = new THREE.PointLight(0xffffff, 5.0);
         this.pointLight.position.copy(new THREE.Vector3(0.0, 1.0, 0.1));
         this.add(this.pointLight);
 
         /**
          * This VLabScene meshes references
          */
-        this.LamborghiniMiura1971P400SV = this.getObjectByName('LamborghiniMiura1971P400SV');
-        this.LamborghiniMiura1971P400SV.position.add(new THREE.Vector3(0.0, 0.2, 0.0));
+        this.LamborghiniMiura1971P400SV = {
+            chassis: this.getObjectByName('LamborghiniMiura1971P400SV'),
+            wheelFL: this.getObjectByName('LamborghiniMiura1971P400SV').getObjectByName('wheelFL'),
+            wheelFR: this.getObjectByName('LamborghiniMiura1971P400SV').getObjectByName('wheelFR'),
+            wheelRL: this.getObjectByName('LamborghiniMiura1971P400SV').getObjectByName('wheelRL'),
+            wheelRR: this.getObjectByName('LamborghiniMiura1971P400SV').getObjectByName('wheelRR')
+        };
+        this.LamborghiniMiura1971P400SV.chassis.position.add(new THREE.Vector3(0.0, 0.2, 0.0));
+this.LamborghiniMiura1971P400SV.chassis.visible = false;
 
         /**
          * Initialize AmmoJS
@@ -52,17 +59,17 @@ class BaseScene extends VLabScene {
          * Physics configuration
          */
 
-        this.AMMO_TRANSFORM_AUX = new Ammo.btTransform();
-        this.AMMO_DISABLE_DEACTIVATION = 4;
-        this.G = -0.2;//-9.82;
-
         this.PhysicsConfiguration = {};
+        this.PhysicsConfiguration.AMMO_TRANSFORM_AUX = new Ammo.btTransform();
+        this.PhysicsConfiguration.AMMO_DISABLE_DEACTIVATION = 4;
+        this.PhysicsConfiguration.G = -9.82;
         this.PhysicsConfiguration.collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
         this.PhysicsConfiguration.dispatcher = new Ammo.btCollisionDispatcher(this.PhysicsConfiguration.collisionConfiguration);
         this.PhysicsConfiguration.broadphase = new Ammo.btDbvtBroadphase();
         this.PhysicsConfiguration.solver = new Ammo.btSequentialImpulseConstraintSolver();
         this.PhysicsConfiguration.physicsWorld = new Ammo.btDiscreteDynamicsWorld(this.PhysicsConfiguration.dispatcher, this.PhysicsConfiguration.broadphase, this.PhysicsConfiguration.solver, this.PhysicsConfiguration.collisionConfiguration);
-        this.PhysicsConfiguration.physicsWorld.setGravity(new Ammo.btVector3(0, this.G, 0));
+        this.PhysicsConfiguration.physicsWorld.setGravity(new Ammo.btVector3(0, this.PhysicsConfiguration.G, 0));
+
 
         this.prepareAmmoJSBodies();
     }
@@ -70,29 +77,25 @@ class BaseScene extends VLabScene {
      * Prepare AmmoJS bodies
      */
     prepareAmmoJSBodies() {
+        this.LamborghiniMiura1971P400SVAmmoJSRaycastVehicle = new AmmoJSUTILS.AmmoJSRaycastVehicle({
+            vLabScene: this,
+            vehiclePos: new THREE.Vector3(0.0, 2.0, 0.0),
+            vehicleQuat: new THREE.Quaternion(0.0, 0.0, 0.0, 1.0),
+            physicsConfiguration: this.PhysicsConfiguration
+        });
+
         this.ammoBodies = [];
         let ammoBody;
 
-        ammoBody = AmmoJSUTILS.ammoBoxBody({
-            mesh: this.LamborghiniMiura1971P400SV,
-            mass: 0.1,
-            friction: 0.1,
-            restitution: 0.9,
-            deactivation: this.AMMO_DISABLE_DEACTIVATION
-        });
-        this.PhysicsConfiguration.physicsWorld.addRigidBody(ammoBody.body);
-        this.ammoBodies.push(ammoBody);
-
 // ground
-this.groundGeometry = new THREE.BoxGeometry(1, 0.001, 1);
-this.groundGeometry.computeBoundingBox();
-var material = new THREE.MeshBasicMaterial({color: 0x454545});
-this.ground = new THREE.Mesh(this.groundGeometry, material);
-this.ground.position.copy(new THREE.Vector3(0.0, -0.05, 0.0));
-this.add(this.ground);
-
-ammoBody = AmmoJSUTILS.ammoBoxBody({
-    mesh: this.ground,
+let groundGeometry = new THREE.BoxGeometry(100, 0.001, 100);
+groundGeometry.computeBoundingBox();
+var groundMaterial = new THREE.MeshLambertMaterial({color: 0x454545});
+let groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+groundMesh.position.copy(new THREE.Vector3(0.0, -0.0005, 0.0));
+this.add(groundMesh);
+ammoBody = AmmoJSUTILS.ammoBoxShapeBody({
+    mesh: groundMesh,
     mass: 0.0,
     friction: 0.1
 });
@@ -100,11 +103,73 @@ this.PhysicsConfiguration.physicsWorld.addRigidBody(ammoBody.body);
 this.ammoBodies.push(ammoBody);
 
 
+//         this.LamborghiniMiura1971P400SVChassisAmmoBody = AmmoJSUTILS.ammoBoxShapeBody({
+//             mesh: this.LamborghiniMiura1971P400SV.chassis,
+//             mass: 0.2,
+//             friction: 0.1,
+//             restitution: 0.9,
+//             deactivation: this.AMMO_DISABLE_DEACTIVATION,
+//             boxMargins: new THREE.Vector3(0.0, 0.005, 0.0)
+//         });
+//         this.PhysicsConfiguration.physicsWorld.addRigidBody(this.LamborghiniMiura1971P400SVChassisAmmoBody.body);
+//         this.ammoBodies.push(this.LamborghiniMiura1971P400SVChassisAmmoBody);
+
+
+// // // dummy
+// // let dummyGeometry = new THREE.BoxGeometry(0.05, 0.03, 0.1);
+// // dummyGeometry.computeBoundingBox();
+// // var dummyMaterial = new THREE.MeshLambertMaterial({color: 0x45ff45});
+// // let dummyMesh = new THREE.Mesh(dummyGeometry, dummyMaterial);
+// // dummyMesh.position.copy(new THREE.Vector3(0.0, 0.1, 0.0));
+// // this.add(dummyMesh);
+// // ammoBody = AmmoJSUTILS.ammoBoxShapeBody({
+// //     mesh: dummyMesh,
+// //     mass: 0.1,
+// //     friction: 0.1
+// // });
+// // this.PhysicsConfiguration.physicsWorld.addRigidBody(ammoBody.body);
+// // this.ammoBodies.push(ammoBody);
+
+
+//         // Raycast Vehicle
+//         let wheelDirectionCS0 = new Ammo.btVector3(0, -1, 0);
+//         let wheelAxleCS = new Ammo.btVector3(-1, 0, 0);
+//         let tuning = new Ammo.btVehicleTuning();
+
+//         let rayCaster = new Ammo.btDefaultVehicleRaycaster(this.PhysicsConfiguration.physicsWorld);
+//         this.LamborghiniMiura1971P400SVVehicle = new Ammo.btRaycastVehicle(tuning, this.LamborghiniMiura1971P400SVChassisAmmoBody, rayCaster);
+//         this.LamborghiniMiura1971P400SVVehicle.setCoordinateSystem(0, 1, 2);
+//         this.PhysicsConfiguration.physicsWorld.addAction(this.LamborghiniMiura1971P400SVVehicle);
+
+
+
+//         var addWheel = function(vehicle, isFront, pos, radius) {
+//             var wheelInfo = vehicle.addWheel(
+//                             pos,
+//                             wheelDirectionCS0,
+//                             wheelAxleCS,
+//                             0.6,
+//                             radius,
+//                             tuning,
+//                             isFront);
+//             // wheelInfo.set_m_suspensionStiffness(suspensionStiffness);
+//             // wheelInfo.set_m_wheelsDampingRelaxation(suspensionDamping);
+//             // wheelInfo.set_m_wheelsDampingCompression(suspensionCompression);
+//             // wheelInfo.set_m_frictionSlip(friction);
+//             // wheelInfo.set_m_rollInfluence(rollInfluence);
+//         }
+
+//         addWheel(this.LamborghiniMiura1971P400SVVehicle, true, this.LamborghiniMiura1971P400SV.wheelFL.position, 0.012);
+//         addWheel(this.LamborghiniMiura1971P400SVVehicle, true, this.LamborghiniMiura1971P400SV.wheelFR.position, 0.012);
+//         addWheel(this.LamborghiniMiura1971P400SVVehicle, false, this.LamborghiniMiura1971P400SV.wheelRL.position, 0.012);
+//         addWheel(this.LamborghiniMiura1971P400SVVehicle, false, this.LamborghiniMiura1971P400SV.wheelRR.position, 0.012);
+
+
         this.vLab.EventDispatcher.subscribe({
             subscriber: this,
             events: {
                 WebGLRendererCanvas: {
-                    framerequest:         this.onFramerequest
+                    framerequest: this.onFramerequest
                 }
             }
         });
@@ -113,12 +178,15 @@ this.ammoBodies.push(ammoBody);
      * onFramerequest
      */
     onFramerequest(params) {
+
+        this.LamborghiniMiura1971P400SVAmmoJSRaycastVehicle.sync();
+
         this.ammoBodies.forEach(ammoBody => {
             if (ammoBody.mass > 0.0) {
                 let ms = ammoBody.body.getMotionState();
-                ms.getWorldTransform(this.AMMO_TRANSFORM_AUX);
-                var p = this.AMMO_TRANSFORM_AUX.getOrigin();
-                var q = this.AMMO_TRANSFORM_AUX.getRotation();
+                ms.getWorldTransform(this.PhysicsConfiguration.AMMO_TRANSFORM_AUX);
+                var p = this.PhysicsConfiguration.AMMO_TRANSFORM_AUX.getOrigin();
+                var q = this.PhysicsConfiguration.AMMO_TRANSFORM_AUX.getRotation();
                 ammoBody.mesh.position.set(p.x(), p.y(), p.z());
                 ammoBody.mesh.quaternion.set(q.x(), q.y(), q.z(), q.w());
             }
